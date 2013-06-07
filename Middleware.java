f7ea13bab39b19336248cf18f5b6b43183a6d571
rkrepela <rudolf.krepela@student.tgm.ac.at>
2013-06-07 11:10:42 +0200

    leeren Files+Links

---
+++ Middleware.java
@@ -0,0 +1,1 @@
+package java_dbconnection_middleware;
---
+++ Testklasse.java
@@ -0,0 +1,20 @@
+package java_dbconnection_middleware;
+
+/**
+ * 
+ * @author 
+ * 
+ * Quellen:
+ * http://jdbc.postgresql.org/documentation/81/listennotify.html
+ * http://stackoverflow.com/questions/12618915/how-to-implement-a-db-listener-in-java
+ *
+ */
+public class Testklasse{
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
